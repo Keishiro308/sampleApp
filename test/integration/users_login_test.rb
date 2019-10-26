7964135794ebb876c330e27df005b06d99169a3d
Keishiro <kn11308@gmail.com>
2019-10-26 21:09:20 +0900

    Finish user edit, update, index, and destroy actions

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -11,6 +11,7 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
   test "login with invalid information" do
     get login_path
     assert_template 'sessions/new'
+    assert session[:forwarding_url].nil?
     post login_path, params: {session:{email: '', password: ''}}
     assert_template 'sessions/new'
     assert_not flash.empty?
