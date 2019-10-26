6a80b96779724887aa0c5009d56f7949a7c02b23
Keishiro <kn11308@gmail.com>
2019-10-26 23:38:39 +0900

    Add account activation

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   get '/signup', to:'users#new'
   post '/signup', to:'users#create'
   resources :users
+  resources :account_activations, only: [:edit]
   get '/help', to:'static_pages#help'
   get  '/about', to:'static_pages#about'
   get  '/contact', to:'static_pages#contact'
